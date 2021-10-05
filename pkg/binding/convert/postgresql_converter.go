988bc6e031ab473cfb8fb4157472a616364fecf8
Sandip Gahlot <sgahlot@redhat.com>
2021-10-05 14:24:17 -0400

    DBAAS-151: Added support for Postgresql options (that can be given in options file with multiple options separated by & sign)

--- pkg/binding/convert/postgresql_converter.go
+++ pkg/binding/convert/postgresql_converter.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+const (
+	optionsSeparator        = "&" // separator for each option
+	optionKeyValueSeparator = "=" // separator for key/value in each option
+)
+
 var escaper = strings.NewReplacer(` `, `\ `, `'`, `\'`, `\`, `\\`)
 
 // PostgreSQLConnectionStringConverter converts a ServiceBinding object to a DSN connection string
@@ -27,5 +32,17 @@ func (m *PostgreSQLConnectionStringConverter) Convert(binding fileconfig.Service
 		addToParts("password", binding.Properties[keyPassword])
 	}
 	addToParts("dbname", binding.Properties[keyDatabase])
+
+	if binding.Properties[keyOptions] != "" {
+		// multiple options are expected to be separated by & (ampersand) sign, e.g. sslmode=disable&connect_timeout=10
+		options := strings.Split(binding.Properties[keyOptions], optionsSeparator)
+		for _, option := range options {
+			optionKeyValue := strings.Split(option, optionKeyValueSeparator)
+			if len(optionKeyValue) == 2 && len(optionKeyValue[0]) > 0 && len(optionKeyValue[1]) > 0 {
+				addToParts(optionKeyValue[0], optionKeyValue[1])
+			}
+		}
+	}
+
 	return strings.Join(parts, " ")
 }
